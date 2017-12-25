fcfc3c49d62a0d767571c85b1cb954b8b2cf2227
zjun <565061763@qq.com>
2017-12-25 17:29:57 +0800

    重构

--- app/src/main/java/com/fanwe/receiver/MainActivity.java
+++ app/src/main/java/com/fanwe/receiver/MainActivity.java
@@ -1,15 +1,49 @@
 package com.fanwe.receiver;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.fanwe.lib.receiver.FHeadsetPlugReceiver;
+import com.fanwe.lib.receiver.FNetworkReceiver;
 
 public class MainActivity extends AppCompatActivity
 {
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHeadsetPlugReceiver.register(this);
+        mNetworkReceiver.register(this);
+    }
+
+    private FHeadsetPlugReceiver mHeadsetPlugReceiver = new FHeadsetPlugReceiver()
+    {
+        @Override
+        protected void onHeadsetPlugChange(boolean plug)
+        {
+            Log.i(TAG, "onHeadsetPlugChange:" + plug);
+        }
+    };
+
+    private FNetworkReceiver mNetworkReceiver = new FNetworkReceiver()
+    {
+        @Override
+        protected void onNetworkChanged(int type)
+        {
+            Log.i(TAG, "onNetworkChanged:" + type);
+        }
+    };
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        mHeadsetPlugReceiver.unregister(this);
+        mNetworkReceiver.unregister(this);
     }
 }
